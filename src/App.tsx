539ac2f947b9f2e4369fdd310ca9426bc7705293
Nicolo Ridulfo <nicoloridulfo@gmail.com>
2022-07-10 17:40:35 +0200

    Made stopping more stable

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,38 @@ import './App.css';
 
 function App() {
   const [on, setOn] = useState(false);
-  const [checked, setChecked] = useState<bigint[]>([]);
-  const [intervalState, setIntervalState] = useState<NodeJS.Timer>()
+  const [primes, setPrimes] = useState<bigint[]>([]);
+  const [running, setRunning] = useState(false);
   const [size, setSize] = useState(4096);
-  const postPrime = (prime: bigint) => {
-    fetch(`https://vol-be.azurewebsites.net/primes/${prime.toString()}`, {
+
+
+  const findPrime = async () => {
+    const number = await bigintCryptoUtils.prime(size);
+    fetch(`https://vol-be.azurewebsites.net/primes/${number.toString()}`, {
       method: 'POST',
     })
-  }
-
-  const addPrime = async () => {
-    const number = await bigintCryptoUtils.prime(size);
-    postPrime(number);
-    setChecked((checked) => [...checked, number]);
+    setPrimes((checked) => [...checked, number]);
+    setRunning(false);
   }
 
   useEffect(() => {
-    if (on) {
-      setIntervalState(setInterval(addPrime, 1000));
-    } else {
-      clearInterval(intervalState);
+    if (on && !running) {
+      setRunning(true);
+      setTimeout(findPrime, 1000);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [on])
+  },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [on, running]);
 
   return (
     <div className="App">
       <h1>Let's find primes together! v0.1</h1>
-      <h3>You have found {checked.length} primes!</h3>
+      <h3>You have found {primes.length} primes!</h3>
       Searching for <input style={{ width: "45px" }} type="number" value={size} onChange={(e) => setSize(Number(e.target.value))} /> Bit primes
       <br></br>
       <br></br>
       <button onClick={() => setOn((state) => !state)}>{on ? 'Stop' : 'Start'}</button>
-      {checked?.map((x, i) => <div key={i}>{x.toString()}</div>)}
+      {primes?.map((x, i) => <div key={i}>{x.toString()}</div>)}
     </div>
   );
 }
