df408526691772d1eb9c6a92f8afcd3b0e751c22
Nicolo Ridulfo <nicoloridulfo@gmail.com>
2022-07-10 15:44:17 +0200

    Deploying version 0.1

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,11 @@ function App() {
 
   return (
     <div className="App">
-      <input type="number" value={size} onChange={(e) => setSize(Number(e.target.value))} />
+      <h1>Let's find primes together! v0.1</h1>
+      <h3>You have found {checked.length} primes!</h3>
+      Searching for <input style={{ width: "45px" }} type="number" value={size} onChange={(e) => setSize(Number(e.target.value))} /> Bit primes
+      <br></br>
+      <br></br>
       <button onClick={() => setOn((state) => !state)}>{on ? 'Stop' : 'Start'}</button>
       {checked?.map((x, i) => <div key={i}>{x.toString()}</div>)}
     </div>
