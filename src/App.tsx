822be7bec0f11b5bc0efaa395e997aee75f458db
Nicolo Ridulfo <nicoloridulfo@gmail.com>
2022-07-10 11:14:23 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as bigintCryptoUtils from 'bigint-crypto-utils';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [on, setOn] = useState(false);
+  const [checked, setChecked] = useState<bigint[]>([]);
+  const [intervalState, setIntervalState] = useState<NodeJS.Timer>()
+  const [size, setSize] = useState(4096);
+  const postPrime = (prime: bigint) => {
+    fetch(`https://vol-be.azurewebsites.net/primes/${prime.toString()}`, {
+      method: 'POST',
+    })
+  }
+
+  const addPrime = async () => {
+    const number = await bigintCryptoUtils.prime(size);
+    postPrime(number);
+    setChecked((checked) => [...checked, number]);
+  }
+
+  useEffect(() => {
+    if (on) {
+      setIntervalState(setInterval(addPrime, 1000));
+    } else {
+      clearInterval(intervalState);
+    }
+  }, [on])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="number" value={size} onChange={(e) => setSize(Number(e.target.value))} />
+      <button onClick={() => setOn((state) => !state)}>{on ? 'Stop' : 'Start'}</button>
+      {checked?.map((x, i) => <div key={i}>{x.toString()}</div>)}
     </div>
   );
 }
